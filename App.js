b0813a6a89b5cd57357d6c31689f87ecdc2f6ce4
lexobegin <65969094+lexobegin@users.noreply.github.com>
2020-12-15 17:25:40 -0400

    5-tenedores

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 //import { StatusBar } from 'expo-status-bar';/* ojo */
 import React from 'react';
 import { YellowBox } from "react-native";
-//import {firebaseApp} from "./app/utils/firebase";
+import {firebaseApp} from "./app/utils/firebase";
 import Navigation from "./app/navigations/Navigation";
 import { decode, encode } from "base-64";
 
